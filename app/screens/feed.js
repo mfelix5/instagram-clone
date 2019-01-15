c12f29d7bec10e9a5adf998de8584d5b6a60b2e9
mfelix5 <mikefelix@gmail.com>
2019-01-15 13:46:24 -0500

    feed now fetches from database

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -16,6 +16,45 @@ export default class feed extends React.Component {
         this.loadFeed();
     }
 
+    timeConverter = (timestamp) => {
+        var a = new Date(timestamp * 1000);
+        var seconds = Math.floor((new Date() - a) / 1000);
+        
+        var interval = Math.floor(seconds / 31536000); // how many years ago
+        if (interval > 1) {
+            return interval + ' year' + this.pluralCheck(interval);
+        }
+        
+        interval = Math.floor(seconds / 2592000); // how many months ago
+        if (interval > 1) {
+            return interval + ' month' + this.pluralCheck(interval);
+        }
+
+        interval = Math.floor(seconds / 86400); // how many days ago
+        if (interval > 1) {
+            return interval + ' day' + this.pluralCheck(interval);
+        }
+
+        interval = Math.floor(seconds / 3600); // how many hours ago
+        if (interval > 1) {
+            return interval + ' hour' + this.pluralCheck(interval);
+        }
+
+        interval = Math.floor(seconds / 60); // how many minutes ago
+        if (interval >= 1) {
+            return interval + ' minute' + this.pluralCheck(interval);
+        }
+        return Math.floor(seconds) + ' second' + this.pluralCheck(seconds);                                                  
+    }
+
+    pluralCheck = (s) => {
+        if (s === 1) {
+            return ' ago';
+        } else {
+            return 's ago';
+        }
+    }
+
     loadFeed = () => {
         this.setState({
             refresh: true,
@@ -28,16 +67,17 @@ export default class feed extends React.Component {
             const exists = (snapshot.val() !== null);
             if (exists) data = snapshot.val();
             var photo_feed = that.state.photo_feed;
+
             for (var photo in data) {
                 var photoObj = data[photo];
-                database.ref('users').child('photoObj.author').once('value').then(function (snapshot) {
+                database.ref('users').child(photoObj.author).once('value').then(function (snapshot) {
                     const exists = (snapshot.val() !== null);
                     if (exists) data = snapshot.val();
                     photo_feed.push({
                         id: photo,
                         url: photoObj.url,
                         caption: photoObj.caption,
-                        posted: photoObj.posted,
+                        posted: that.timeConverter(photoObj.posted),
                         author: data.username
                     });
 
@@ -45,58 +85,51 @@ export default class feed extends React.Component {
                         refresh: false,
                         loading: false
                     });
+
                 }).catch(error => console.log(error));
             }
-        }).catch(error => console.log(error);
+        }).catch(error => console.log(error));
     }
 
     loadNew = () => {
-        this.setState({
-            refresh: true
-        });
-        this.setState({
-            photo_feed: [5, 6, 7, 8, 9],
-            refresh: false
-        });
+        this.loadFeed();
     }
 
     render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <View style={{ height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightgray', borderBottomWidth: 0.5, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Feed</Text>
-                </View>
-
-                <FlatList
-                    refreshing={this.state.refresh}
-                    onRefresh={this.loadNew}
-                    data={this.state.photo_feed}
-                    keyExtractor={(item, index) => index.toString()}
-                    style={{ flex: 1, backgroundColor: '#eee' }}
-                    renderItem={({ item, index }) => (
-                        <View
-                            key={index}
-                            style={{ width: '100%', overflow: 'hidden', marginBottom: 5, justifyContent: 'space-between', borderBottomWidth: 1, borderColor: 'grey' }}
-                        >
-                            <View
-                                style={{ padding: 5, width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <Text>Time ago</Text>
-                                <Text>@posted by</Text>
-                            </View>
-                            <View>
-                                <Image
-                                    source={{ uri: 'https://source.unsplash.com/random/500x' + Math.floor((Math.random() * 800 + 500)) }}
-                                    style={{ resizeMode: 'cover', width: '100%', height: 275 }}
-                                />
-                            </View>
-                            <View style={{ padding: 5 }}>
-                                <Text>Caption text here...</Text>
-                                <Text style={{ marginTop: 10, textAlign: 'center' }}>View comments...</Text>
-                            </View>
-                        </View>
-                    )}
-                />
+        return <View style={{ flex: 1 }}>
+            <View style={{ height: 70, paddingTop: 30, backgroundColor: "white", borderColor: "lightgray", borderBottomWidth: 0.5, justifyContent: "center", alignItems: "center" }}>
+              <Text>Feed</Text>
             </View>
-        )
+
+            {this.state.loading === true ? <View style={{ color: "green", flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text>Loading...</Text>
+              </View> : 
+              
+              <FlatList 
+                refreshing={this.state.refresh} 
+                onRefresh={this.loadNew} 
+                data={this.state.photo_feed} 
+                keyExtractor={(item, index) => index.toString()} 
+                style={{ flex: 1, backgroundColor: "#eee" }} 
+                renderItem={({ item, index }) => 
+                
+                    <View key={index} style={{ width: "100%", overflow: "hidden", marginBottom: 5, justifyContent: "space-between", borderBottomWidth: 1, borderColor: "grey" }}>
+                    <View style={{ padding: 5, width: "100%", flexDirection: "row", justifyContent: "space-between" }}>
+                      <Text>{item.posted}</Text>
+                      <Text>{item.author}</Text>
+                    </View>
+                    <View>
+                      <Image source={{ uri: item.url }} style={{ resizeMode: "cover", width: "100%", height: 275 }} />
+                    </View>
+                    <View style={{ padding: 5 }}>
+                      <Text>{item.caption}</Text>
+                      <Text
+                        style={{ marginTop: 10, textAlign: "center" }}
+                      >
+                        View comments...
+                      </Text>
+                    </View>
+                  </View>} />}
+          </View>;
     }
 }
